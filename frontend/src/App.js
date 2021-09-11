c4fd428415435641bf036f91a79ec365e05ec866
Shikai <shikaijin7@gmail.com>
2021-09-10 22:16:18 -0400

    feat: add background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,19 @@ import React from "react";
 import { makeStyles } from "@material-ui/styles";
 import { Box } from "@material-ui/core";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import logo from "../src/static/images/logo.png";
+import logo from "../src/static/images/Rectangle.png";
+import layer from "../src/static/images/Website.png";
 import Link from "@material-ui/core/Link";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    backgroundColor: "#fff",
     display: "flex",
     flexWrap: "wrap",
     justifyContent: "center",
     marginBottom: "2rem",
+    backgroundImage: `url(${layer})`,
+    backgroundSize: "100% 100%",
+    backgroundPosition: "center",
   },
   intro: {
     width: 700,
@@ -39,8 +42,9 @@ const useStyles = makeStyles((theme) => ({
     width: 700,
     minWidth: "40%",
     maxWidth: "100%",
+    height: "75vh",
     backgroundImage: `url(${logo})`,
-    backgroundSize: "cover",
+    backgroundSize: "100% 100%",
     backgroundPosition: "center",
   },
   contact: {
@@ -65,7 +69,7 @@ function App() {
       <div>
         <Box className={classes.root} width="100%">
           {/* Intro unit */}
-          <Box className={classes.intro} style={{ height: "10vh" }}>
+          <Box className={classes.intro}>
             <Typography variant="h5" className={classes.title}>
               Hello,
               <Typography variant="h3">I'm Shikai Jin</Typography>
@@ -88,11 +92,11 @@ function App() {
             </Typography>
             {/* End shortcuts unit */}
           </Box>
-          <Box
-            className={classes.image}
-            style={{ height: "75vh", margin: 0, padding: 0 }}
-          />
+          {/* Logo unit */}
+          <Box className={classes.image} />
         </Box>
+        {/* End logo unit */}
+        {/* Contact unit */}
         <Box className={classes.contact} width="100%">
           <Box className={classes.email}>
             <Typography variant="h5">
@@ -107,6 +111,7 @@ function App() {
             </Typography>
           </Box>
         </Box>
+        {/* End contact unit */}
       </div>
     </React.Fragment>
   );
